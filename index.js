9f3f61625a2533b980dcb4901d72f363dc32dffc
AhmetEjler <ahmetejler@gmail.com>
2026-09-11 15:32:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/welcome', async (req, res) => {
 
         // Resim linkini döndür
         const imageUrl = `https://${req.get('host')}/uploads/${fileName}`;
-        res.json({ url: imageUrl });
+        res.send(imageUrl);
     } catch (error) {
         console.error(error);
         res.status(500).send('Kart oluşturulamadı');
