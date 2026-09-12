1ed63017e54f16b83abd6a6b82a9308b3b4cd972
AhmetEjler <ahmetejler@gmail.com>
2026-09-12 13:52:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.all('/welcome', async (req, res) => {
 
         const avatarImage = await loadImage(avatar);
         const avatarSize = 160;
-        const avatarCenterX = 510;
+        const avatarCenterX = 509;
         const avatarCenterY = 212;
         const avatarX = avatarCenterX - (avatarSize / 2);
         const avatarY = avatarCenterY - (avatarSize / 2);
@@ -107,8 +107,8 @@ app.all('/goodbye', async (req, res) => {
 
         const avatarImage = await loadImage(avatar);
         const avatarSize = 160;
-        const avatarCenterX = 510;
-        const avatarCenterY = 225;
+        const avatarCenterX = 509;
+        const avatarCenterY = 223;
         const avatarX = avatarCenterX - (avatarSize / 2);
         const avatarY = avatarCenterY - (avatarSize / 2);
 
