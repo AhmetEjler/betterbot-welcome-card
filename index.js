d6b8f3ce5f937b43f8b0fc7b0230e36ef850f7d9
AhmetEjler <ahmetejler@gmail.com>
2026-09-12 13:50:13 +0300

    Fix avatar center alignment in index.js
    Adjusted avatarCenterX value for better alignment.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.all('/welcome', async (req, res) => {
 
         const avatarImage = await loadImage(avatar);
         const avatarSize = 160;
-        const avatarCenterX = 512;
+        const avatarCenterX = 510;
         const avatarCenterY = 212;
         const avatarX = avatarCenterX - (avatarSize / 2);
         const avatarY = avatarCenterY - (avatarSize / 2);
@@ -107,7 +107,7 @@ app.all('/goodbye', async (req, res) => {
 
         const avatarImage = await loadImage(avatar);
         const avatarSize = 160;
-        const avatarCenterX = 512;
+        const avatarCenterX = 510;
         const avatarCenterY = 225;
         const avatarX = avatarCenterX - (avatarSize / 2);
         const avatarY = avatarCenterY - (avatarSize / 2);
